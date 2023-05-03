bbb5c2b63a9610e830ec4c8f5d6c596ceb69db93
NSergeevich87 <niknikitaser@gmail.com>
2023-05-03 12:53:23 +0200

    lection_56

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Reflection;
 
 namespace Lesson
 {
-    enum Color
+    // Модификаторы доступа public и private, для членов класса
+
+   /* enum Color
     {
         Red, 
         Green, 
@@ -12,35 +16,60 @@ namespace Lesson
         Magenta, 
         Orange, 
         Yellow
-    }
+    }*/
     class Point
     {
-        public int x; 
-        public int y;
-        public Color color;
+        int z = 3;
+        public int x = 1; 
+        private int y = 44;
+
+        private void PrintX()
+        {
+            Console.WriteLine($"X: {x}");
+        }
+
+        public void PrintY()
+        {
+            Console.WriteLine($"Y: {y}");
+        }
+
+        public void PrintZ()
+        {
+            Console.WriteLine($"Z: {z}");
+        }
+
+        public void PrintPoint()
+        { 
+            PrintX();
+            PrintY();
+            PrintZ();
+        }
+        //public Color color;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            /*Point p = new Point();
-            p.x = 4;
-            p.y = 2;
+            Point point = new Point();
 
-            Point p2 = new Point();
-            p2.x = 2;
-            p2.y = 6;
+            point.PrintZ();
 
-            Console.WriteLine($"X: {p.x} | Y: {p.y}");*/
+            var typeInfo = typeof(Point).
+                GetFields(BindingFlags.Instance | 
+                BindingFlags.NonPublic | 
+                BindingFlags.Public);
 
-            Point p = new Point();
+            foreach (var item in typeInfo)
+            {
+                Console.WriteLine($"{item.Name}\t IsPrivate: {item.IsPrivate}\t IsPublic: {item.IsPublic}");
+            }
 
-            p.x = 4;
+            /*p.x = 4;
             p.y = 2;
 
             p.color = Color.Magenta;
 
-            Console.WriteLine($"X: {p.x} | Y: {p.y} | Color: {p.color}");
+            Console.WriteLine($"X: {p.x} | Y: {p.y} | Color: {p.color}");*/
         }
     }
 }
